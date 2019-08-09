2b5a4c45629b60d0fd478fef6b164db48a943087
izuua <izuuaaf@gmail.com>
2019-08-08 23:37:00 -0400

    Added the basic files and have the server running.

---
+++ app/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+require("./routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`App listening on PORT: ${PORT}`);
+})
