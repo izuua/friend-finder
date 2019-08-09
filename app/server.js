4a160daadc2fda2cc40075f79a4222caef66c94e
izuua <izuuaaf@gmail.com>
2019-08-09 01:08:33 -0400

    Added paths to the api data.

--- app/server.js
+++ app/server.js
@@ -7,7 +7,9 @@ var app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
+
 
 app.listen(PORT, function() {
     console.log(`App listening on PORT: ${PORT}`);
